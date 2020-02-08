19592caa115f9084240b154800096c58a3160851
sash <sash2104@gmail.com>
2020-02-08 15:44:45 +0900

    :tada: update example test

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-exe=$1
+run=$1
 indir=$2
 outdir=$3
+checker="dummy"
+if [[ $# -eq 4 ]]; then
+  checker=$4
+fi
 
 if [[ ! -d $outdir ]]; then
   mkdir -p $outdir
 fi
 
-for file in $(ls $indir/*.in)
+result=$3/result.txt
+echo -n > $result
+
+for i in $(seq 1 10)
 do
-  filebase=$(basename ${file%.*})
+  file=$indir/$i.in
+  filebase=$(basename ${file%.*}) # 現状はiと同じになる
   outfile=$outdir/${filebase}.out
   errfile=$outdir/${filebase}.err
-  echo "$exe < $file > $outfile 2> $errfile"
-  $exe < $file > $outfile 2> $errfile
+  echo "$run < $file > $outfile 2> $errfile" >&2
+  $run < $file > $outfile 2> $errfile
   head $errfile
   tail $errfile
+  if [[ $# -eq 4 ]]; then
+    ansfile=$indir/$i.ans
+    $checker $file $outfile $ansfile >> $result
+  fi
 done
+cat $result
