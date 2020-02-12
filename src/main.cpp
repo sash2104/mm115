061a1572c20a718c9bdc04255c664fc6657021f3
sash <sash2104@gmail.com>
2020-02-12 22:31:18 +0900

    :construction: 辺追加、削除、変更の焼きなまし

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 // C++11
 #include <cassert>
+#include <cmath>
 #include <cstdlib>
 #include <sys/time.h>
 #include <algorithm>
@@ -30,6 +31,27 @@ struct Timer {
     return ((int64_t)low) | ((int64_t)high << 32);
   }
 };
+
+struct XorShift {
+  unsigned int x, y, z, w;
+  double nL[65536];
+  XorShift() { init(); }
+  void init()
+  {
+    x = 314159265; y = 358979323; z = 846264338; w = 327950288;
+    double n = 1 / (double)(2 * 65536);
+    for (int i = 0; i < 65536; i++) {
+      nL[i] = log(((double)i / 65536) + n);
+    }
+  }
+  inline unsigned int next() { unsigned int t=x^x<<11; x=y; y=z; z=w; return w=w^w>>19^t^t>>8; }
+  inline double nextLog() { return nL[next()&0xFFFF]; }
+  inline int nextInt(int m) { return (int)(next()%m); } // [0, m)
+  int nextInt(int min, int max) { return min+nextInt(max-min+1); } // [min, max]
+  inline double nextDouble() { return (double)next()/((long long)1<<32); } // [0, 1]
+};
+
+XorShift rnd;
 
 namespace logger {
 inline void json_() {}
@@ -110,6 +132,257 @@ int matrix[N_MAX*N_MAX]; // (x,y)が繋がっている -> matrix[xy2id(x,y)] = 1
 int dgold[N_MAX*N_MAX]; // matrix[xy2id(x,y)] = (x,y)間の与えられた距離. 情報なしはINF 
 int dpred[N_MAX*N_MAX]; // matrix[xy2id(x,y)] = (x,y)間の推測した距離. 未確定はINF
 bool done[N_MAX*N_MAX]; // done[xy2id(x,y)] = (x,y)間のdpredが確定したならtrue
+
+struct State {
+  int invalid = -10000;
+  int bid, bscore, br, bid2; // backup
+  int score;
+  vector<int> lv;
+  vector<int> fixed_ids, free_ids, used_ids;
+  int V, E;
+  vector<bool> used, fixed;
+  unordered_map<int, int> rid;
+  State() {}
+  State(const vector<int> &lv, const vector<int> &fixed_ids, const vector<int> &free_ids): 
+    lv(lv), fixed_ids(fixed_ids), free_ids(free_ids), V(lv.size()), E(free_ids.size()), used(N_MAX*N_MAX,false), fixed(N_MAX*N_MAX,false) {
+    for (int i = 0; i < V; ++i) { rid[lv[i]] = i; }
+  }
+
+  int update() {
+    int j = rnd.nextInt(10);
+    if (j > 5 && used_ids.size() > 0 && free_ids.size() > 1) {
+      // 辺の入れ替え
+      br = 1;
+      int i = rnd.nextInt(used_ids.size());
+      int id = used_ids[i];
+      int i2 = rnd.nextInt(E);
+      int id2 = free_ids[i2];
+      while (fixed[id2] && id2 == id && used[id2]) { 
+        i2 = rnd.nextInt(E);
+        id2 = free_ids[i2];
+      }
+      return update(id, id2);
+    }
+    else {
+      br = 2;
+      int i = rnd.nextInt(E);
+      int id = free_ids[i];
+      while (fixed[id]) { 
+        i = rnd.nextInt(E);
+        id = free_ids[i];
+      }
+      return update(id);
+    }
+  }
+
+  int update(int id, int id2) {
+    // cerr << "[upd2]" << id << " " << id2 << endl;
+    // backup
+    bid = id;
+    bid2 = id2;
+    bscore = score;
+    {
+      int rev = revid(id);
+      if (used[id]) { // deletion
+        used_ids.erase(std::remove(used_ids.begin(), used_ids.end(), id), used_ids.end());
+        used_ids.erase(std::remove(used_ids.begin(), used_ids.end(), rev), used_ids.end());
+      }
+      else { // insertion
+        used_ids.push_back(id);
+        used_ids.push_back(rev);
+      }
+      // 辺の使用/不使用を反転させる
+      used[id] = !used[id];
+      used[rev] = !used[rev];
+    }
+    {
+      int rev = revid(id2);
+      if (used[id2]) { // deletion
+        used_ids.erase(std::remove(used_ids.begin(), used_ids.end(), id2), used_ids.end());
+        used_ids.erase(std::remove(used_ids.begin(), used_ids.end(), rev), used_ids.end());
+      }
+      else { // insertion
+        used_ids.push_back(id2);
+        used_ids.push_back(rev);
+      }
+      // 辺の使用/不使用を反転させる
+      used[id2] = !used[id2];
+      used[rev] = !used[rev];
+    }
+    int tmp = calcScore();
+    if (tmp == invalid) return invalid;
+    score = tmp;
+    return score-bscore;
+  }
+  int update(int id) {
+    // backup
+    bid = id;
+    bscore = score;
+
+    int rev = revid(id);
+    if (used[id]) { // deletion
+      used_ids.erase(std::remove(used_ids.begin(), used_ids.end(), id), used_ids.end());
+      used_ids.erase(std::remove(used_ids.begin(), used_ids.end(), rev), used_ids.end());
+    }
+    else { // insertion
+      used_ids.push_back(id);
+      used_ids.push_back(rev);
+    }
+    // 辺の使用/不使用を反転させる
+    used[id] = !used[id];
+    used[rev] = !used[rev];
+    // cerr << "[upd]" << id << " " << used_ids.size() << endl;
+
+    int tmp = calcScore();
+    if (tmp == invalid) return invalid;
+    score = tmp;
+    return score-bscore;
+  }
+
+  int calcScore() {
+    // graphの構築
+    vector<vector<int>> g(V);
+    for (int id: fixed_ids) {
+      int v1 = rid[id2x(id)];
+      int v2 = rid[id2y(id)];
+      g[v1].push_back(v2);
+      g[v2].push_back(v1);
+    }
+    for (int id: used_ids) {
+      int v1 = rid[id2x(id)];
+      int v2 = rid[id2y(id)];
+      g[v1].push_back(v2);
+      g[v2].push_back(v1);
+    }
+    // bfs
+    vector<vector<int>> dist(V, vector<int>(V, INF));
+    for (int v = 0; v < V; ++v) {
+      dist[v][v] = 0;
+      queue<int> q;
+      q.push(v);
+      vector<bool> visit(lv.size(), false);
+      visit[v] = true;
+      while (!q.empty()) { 
+        int v1 = q.front(); q.pop();
+        for (int v2 : g[v1]) {
+          if (visit[v2]) continue;
+          int id = xy2id(lv[v1], lv[v2]);
+          visit[v2] = true;
+          dist[v][v2] = dist[v][v1]+1;
+          // if (dgold[id] != INF && dist[v][v2] < dgold[id]) return -1;
+          q.push(v2);
+        }
+      }
+    }
+
+    // 実際に判明している距離との差分を計算
+    int ret = 0;
+    for (int v = 0; v < V; ++v) {
+      for (int v2 = 0; v2 < V; ++v2) {
+        if (v == v2) continue;
+        int id = xy2id(lv[v], lv[v2]);
+        if (dist[v][v2] == INF || dgold[id] == NG) {
+          continue;
+        }
+        if (dgold[id] == INF) {
+          if (dist[v][v2] < INF) --ret; // 未確定の部分を確定しない辺を優先したい
+          continue;
+        }
+        if (dist[v][v2] < dgold[id]) {
+          // cerr << v << " " << v2 << " " << dist[v][v2] << " " << dgold[id] << endl;
+          return -10000;
+        }
+        if (dist[v][v2] == dgold[id]) {
+          ret += 100;
+        }
+      }
+    }
+    return ret;
+  }
+
+  void revert() { 
+    score = bscore;
+    {
+      int rev = revid(bid);
+      if (used[bid]) { // deletion
+        used_ids.erase(std::remove(used_ids.begin(), used_ids.end(), bid), used_ids.end());
+        used_ids.erase(std::remove(used_ids.begin(), used_ids.end(), rev), used_ids.end());
+      }
+      else { // insertion
+        used_ids.push_back(bid);
+        used_ids.push_back(rev);
+      }
+      used[bid] = !used[bid];
+      used[rev] = !used[rev];
+    }
+    if (br == 1) {
+      // cerr << "[rvt2]" << bid << " " << bid2 << endl;
+      int rev = revid(bid2);
+      if (used[bid2]) { // deletion
+        used_ids.erase(std::remove(used_ids.begin(), used_ids.end(), bid2), used_ids.end());
+        used_ids.erase(std::remove(used_ids.begin(), used_ids.end(), rev), used_ids.end());
+      }
+      else { // insertion
+        used_ids.push_back(bid2);
+        used_ids.push_back(rev);
+      }
+      used[bid2] = !used[bid2];
+      used[rev] = !used[rev];
+    }
+    // cerr << "[rvt]" << bid << " " << used_ids.size() << endl;
+  }
+
+  void write() {
+  }
+};
+
+struct SASolver {
+  double startTemp = 30;
+  double endTemp = 10;
+  Timer timer = Timer(0.95);
+  State best;
+  SASolver() { init(); }
+  SASolver(double st, double et): startTemp(st), endTemp(et) { init(); }
+  SASolver(double st, double et, double limit): startTemp(st), endTemp(et), timer(limit) { init(); }
+  void init() {
+    logger::json("tag", "param", "type", "sa", "start_temp", startTemp, "end_temp", endTemp, "limit", timer.LIMIT);
+  } // 初期化処理をここに書く
+
+  void solve(State &state) {
+    double t;
+    double score = state.calcScore();
+    best = state;
+    double bestScore = score;
+    int counter = 0;
+    while ((t = timer.get()) < timer.LIMIT) // 焼きなまし終了時刻までループ
+    {
+      for (int i = 0; i < 1000; ++i) { // 時間計算を間引く
+        int diff = state.update();
+        if (diff == -10000) { // 絶対に更新しない場合
+          state.revert();
+          continue;
+        }
+
+        // 最初t=0のときは、スコアが良くなろうが悪くなろうが、常にnextを使用
+        // 最後t=timer.LIMITのときは、スコアが改善したときのみ、nextを使用
+        double T = startTemp + (endTemp - startTemp) * t / timer.LIMIT;
+        // スコアが良くなった or 悪くなっても強制遷移
+        if (diff >= T*rnd.nextLog())
+        {
+          score += diff;
+          if (bestScore < score) {
+            bestScore = score;
+            best = state;
+            logger::json("tag", "!", "time", t, "counter", counter, "score", score, "e", best.used_ids.size());
+          }
+        }
+        else { state.revert(); }
+        ++counter;
+      }
+    }
+    logger::json("tag", "result", "counter", counter, "score", bestScore);
+  }
+};
 
 struct Solver {
   int N;
@@ -208,7 +481,7 @@ struct Solver {
       bruteforce(lv);
     }
     // 貪欲に辺を拡張する
-    expand_edges();
+    // expand_edges();
   }
 
   void expand_edges() {
@@ -382,45 +655,51 @@ struct Solver {
       // cerr << best_dist << " " << best.size() << endl;
     }
     else { 
-      // lv.size() >= 8
-      if (K < 3 && lv.size() < 30) return;
-      vector<bool> used(n, false);
-      vector<int> best2;
-      int cnt = 0;
-      while (true) {
-        double t = timer.get();
-        // cerr << t << endl;
-        if (t > timer.LIMIT) break;
-        int best_i = -1;
-        int tmp_best = best_dist;
-        vector<int> local_best;
-        for (int i = 0; i < n; ++i) { 
-          if (used[i]) continue;
-          vector<int> ids;
-          ids.push_back(free_ids[i]);
-          ids.push_back(revid(free_ids[i]));
-          int ret = bfs(lv, connected_ids, ids);
-          if (best_dist < ret) {
-            best_dist = ret;
-            local_best = ids;
-            best_i = i;
-            // cerr << best_dist << endl;
-          }
-        }
-
-        if (tmp_best == best_dist) break;
-        if (local_best.size() > 0) {
-          used[best_i] = true;
-          for (int id: local_best) {
-            connected_ids.push_back(id);
-            best2.push_back(id);
-            // best.push_back(id);
-          }
-        }
-      }
-      for (int id : best2) {
+      State s(lv, connected_ids, free_ids);
+      SASolver sa;
+      sa.solve(s);
+      for (int id : sa.best.used_ids) {
         best.push_back(id);
       }
+      // lv.size() >= 8
+      // if (K < 3 && lv.size() < 30) return;
+      // vector<bool> used(n, false);
+      // vector<int> best2;
+      // int cnt = 0;
+      // while (true) {
+      //   double t = timer.get();
+      //   // cerr << t << endl;
+      //   if (t > timer.LIMIT) break;
+      //   int best_i = -1;
+      //   int tmp_best = best_dist;
+      //   vector<int> local_best;
+      //   for (int i = 0; i < n; ++i) { 
+      //     if (used[i]) continue;
+      //     vector<int> ids;
+      //     ids.push_back(free_ids[i]);
+      //     ids.push_back(revid(free_ids[i]));
+      //     int ret = bfs(lv, connected_ids, ids);
+      //     if (best_dist < ret) {
+      //       best_dist = ret;
+      //       local_best = ids;
+      //       best_i = i;
+      //       // cerr << best_dist << endl;
+      //     }
+      //   }
+
+      //   if (tmp_best == best_dist) break;
+      //   if (local_best.size() > 0) {
+      //     used[best_i] = true;
+      //     for (int id: local_best) {
+      //       connected_ids.push_back(id);
+      //       best2.push_back(id);
+      //       // best.push_back(id);
+      //     }
+      //   }
+      // }
+      // for (int id : best2) {
+      //   best.push_back(id);
+      // }
     }
     for (int id : best) { 
       matrix[id] = 1;
